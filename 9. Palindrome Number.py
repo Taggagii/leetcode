3987475c6639fdf97e1e75288bc17e2fd4637527
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2022-11-21 02:44:40 -0500

    removing old solution

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -24,30 +24,12 @@
 
 #         return x == stack;
 
-# import math
+import math
 
-# class Solution(object):
-#     def lemma(self, x):
-#         return int(math.log(x, 10)) + 1
-    
-#     def isPalindrome(self, x):
-#         """
-#         :type x: int
-#         :rtype: bool
-#         """
-#         if x < 0:
-#             return False
-#         if not x:
-#             return True
-#         stack = 0
-#         tempX = x
-#         length = self.lemma(x)
-#         for _ in range(length):
-#             tempX, mod = divmod(tempX, 10)
-#             stack = stack * 10 + mod
-#         return stack == x
-        
 class Solution(object):
+    def lemma(self, x):
+        return int(math.log(x, 10)) + 1
+    
     def isPalindrome(self, x):
         """
         :type x: int
@@ -55,21 +37,15 @@ class Solution(object):
         """
         if x < 0:
             return False
-        stringX = str(x)
-        listX = []
-        #populates listX
-        for i in stringX:
-            listX += i
-        print(listX)
-        #duplicate and reverse listX
-        reverseListX = listX.copy()
-        reverseListX.reverse()
-        
-        for i in range(len(listX)):
-            if listX[i] != reverseListX[i]:
-                return False
-            
-        return True
+        if not x:
+            return True
+        stack = 0
+        tempX = x
+        length = self.lemma(x)
+        for _ in range(length):
+            tempX, mod = divmod(tempX, 10)
+            stack = stack * 10 + mod
+        return stack == x
 
     
 s = Solution()
